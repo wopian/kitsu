a3325fda580d18802439a1f74766edcf7c33cbf8
James Harris <wopian@wopian.me>
2017-07-18 06:21:21 +0100

    feat: simplify remove method

--- src/methods/remove.js
+++ src/methods/remove.js
@@ -6,24 +6,23 @@ import { serialise } from '../util'
  * Delete an existing resource - aliases: `destroy`
  * @memberof Kitsu
  * @param {String} model Model to update a resource under
- * @param {Object} data Data to send in a request
+ * @param {String|Number} id ID of the resource to delete
  *
  * @example
  * // Delete a user's post
- * kitsu.remove('posts', {
- *   id: '12345678',
- * })
+ * kitsu.remove('posts', 123)
  */
-export default async function (model, data) {
+export default async function (model, id) {
   try {
     if (!this.isAuth) throw new Error('Not authenticated')
-    if (typeof data.id === 'undefined') throw new Error('PATCH request is missing a model ID')
+    if (typeof id === 'undefined') throw new Error('PATCH request is missing a model ID')
     // Handle request
     const opts = Object.assign({
-      body: JSON.stringify(serialise(model, data, 'DELETE'))
+      body: JSON.stringify(serialise(model, { id }, 'DELETE'))
     }, this._opts)
-    await r.patch(`${this._apiUrl}/${kebab(model, '-')}/${data.id}`, opts)
-      .catch(e => { throw JSON.parse(e.response.body) })
+    await r.delete(`${this._apiUrl}/${this._apiVer}/${kebab(model, '-')}/${id}`, opts)
+      .then(({ status }) => console.log(status))
+      .catch(e => { throw e.response.body })
   } catch (e) {
     throw e
   }
