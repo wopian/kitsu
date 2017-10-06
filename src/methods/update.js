581805b5674c64b777569c2f6ebbf4a98bbea8b8
James Harris <wopian@wopian.me>
2017-10-06 22:45:10 +0100

    feat: migrate update to axios

--- src/methods/update.js
+++ src/methods/update.js
@@ -1,6 +1,17 @@
-export default async function (model, params) {
+import kebab from 'decamelize'
+import plural from 'pluralize'
+import { serialise } from '../util'
+
+export default async function (model, body) {
   try {
+    if (!this.axios.defaults.headers.Authorization) throw new Error('Not logged in')
+    if (typeof body.id === 'undefined') throw new Error('Updating a resource requires an ID')
+    let { data } = await this.axios.patch(`${plural(kebab(model))}/${body.id}`, {
+      data: await serialise(model, body, 'PATCH')
+    })
+    return data
   } catch (error) {
-    return error
+    const e = error.response.data
+    return e.errors ? e.errors : e
   }
 }
