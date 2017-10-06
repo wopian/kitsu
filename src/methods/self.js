e741b0fc5532f303de45f033b9953af4298135ae
James Harris <wopian@wopian.me>
2017-10-06 22:45:20 +0100

    feat: migrate whoAmI to axios

--- src/methods/self.js
+++ src/methods/self.js
@@ -1,5 +1,9 @@
-export default async function (model, params) {
+import fetch from './fetch'
+
+export default async function (params) {
   try {
+    const { data } = await fetch.bind(this)('users', Object.assign({ filter: { self: true } }, params))
+    return data[0]
   } catch (error) {
     return error
   }
