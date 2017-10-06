3eb7d3354bea91eb01e89c4fd7e56656d62a2439
James Harris <wopian@wopian.me>
2017-10-06 22:45:01 +0100

    feat: migrate remove to axios

--- src/methods/remove.js
+++ src/methods/remove.js
@@ -1,6 +1,17 @@
-export default async function (model, params) {
+import kebab from 'decamelize'
+import plural from 'pluralize'
+import { serialise } from '../util'
+
+export default async function (model, id) {
   try {
+    console.log(await serialise(model, { id }, 'DELETE'))
+    if (!this.axios.defaults.headers.Authorization) throw new Error('Not logged in')
+    let { data } = await this.axios.delete(`${plural(kebab(model))}/${id}`, {
+      data: await serialise(model, { id }, 'DELETE')
+    })
+    return data
   } catch (error) {
-    return error
+    const e = error.response.data
+    return e.errors ? e.errors : e
   }
 }
