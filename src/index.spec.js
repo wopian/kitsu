89f2b45c72d56d4c06a4a426de83221c99d02c54
James Harris <wopian@wopian.me>
2017-12-31 02:53:09 +0000

    test: add basic patch, post and delete tests

--- src/index.spec.js
+++ src/index.spec.js
@@ -6,8 +6,12 @@ import Kitsu from 'kitsu'
 import {
   getCollection,
   getCollectionWithIncludes,
+  getError,
   getSingle,
-  getSingleWithIncludes
+  getSingleWithIncludes,
+  getSingleWithNestedIncludes,
+  patchSingle,
+  patchSingleMissingID
 } from './__cases__'
 
 const mock = new MockAdapter(axios)
@@ -52,29 +56,6 @@ describe('Kitsu Class', () => {
     })
   })
 
-  it('Should return false if Authorization header not set', () => {
-    expect.assertions(1)
-    const api = new Kitsu()
-    expect(api.isAuth).toBe(false)
-  })
-
-  it('Should return true if Authorization header is set in constructor', () => {
-    expect.assertions(1)
-    const api = new Kitsu({
-      headers: {
-        Authorization: 'value'
-      }
-    })
-    expect(api.isAuth).toBe(true)
-  })
-
-  it('Should return true if Authorization header is set by method', () => {
-    expect.assertions(1)
-    const api = new Kitsu()
-    api.headers['Authorization'] = 'value'
-    expect(api.isAuth).toBe(true)
-  })
-
   it('Should use decamelize by default', () => {
     expect.assertions(2)
     const api = new Kitsu()
@@ -82,7 +63,7 @@ describe('Kitsu Class', () => {
     expect(api.kebab('longWord')).toBe('long_word')
   })
 
-  it('Should not use decamlize if disabled in options', () => {
+  it('Should not use decamelize if disabled in options', () => {
     expect.assertions(2)
     const api = new Kitsu({
       decamelize: false
@@ -109,21 +90,38 @@ describe('Kitsu Class', () => {
   })
 })
 
-describe('GET Requests', () => {
+describe('isAuth', () => {
+  it('Should return false if Authorization header not set', () => {
+    expect.assertions(1)
+    const api = new Kitsu()
+    expect(api.isAuth).toBe(false)
+  })
+
+  it('Should return true if Authorization header is set in constructor', () => {
+    expect.assertions(1)
+    const api = new Kitsu({
+      headers: {
+        Authorization: 'value'
+      }
+    })
+    expect(api.isAuth).toBe(true)
+  })
+
+  it('Should return true if Authorization header is set by method', () => {
+    expect.assertions(1)
+    const api = new Kitsu()
+    api.headers['Authorization'] = 'value'
+    expect(api.isAuth).toBe(true)
+  })
+})
+
+describe('get', () => {
   it('Should fetch a collection of resources', async () => {
     expect.assertions(1)
     const api = new Kitsu()
     mock.onGet().reply(200, getCollection.jsonapi)
     const request = await api.get('anime')
     expect(request).toEqual(getCollection.kitsu)
-  })
-
-  it('Should fetch a collection of resources with includes', async () => {
-    expect.assertions(1)
-    const api = new Kitsu()
-    mock.onGet().reply(200, getCollectionWithIncludes.jsonapi)
-    const request = await api.get('anime')
-    expect(request).toEqual(getCollectionWithIncludes.kitsu)
   })
 
   it('Should fetch a single resource', async () => {
@@ -134,23 +132,117 @@ describe('GET Requests', () => {
     expect(request).toEqual(getSingle.kitsu)
   })
 
+  it('Should fetch a collection of resources with includes', async () => {
+    expect.assertions(1)
+    const api = new Kitsu()
+    mock.onGet().reply(200, getCollectionWithIncludes.jsonapi)
+    const request = await api.get('anime', { include: 'author,comments' })
+    expect(request).toEqual(getCollectionWithIncludes.kitsu)
+  })
+
   it('Should fetch a single resource with includes', async () => {
     expect.assertions(1)
     const api = new Kitsu()
     mock.onGet().reply(200, getSingleWithIncludes.jsonapi)
-    const request = await api.get('anime/1')
+    const request = await api.get('anime/1', { include: 'author,comments' })
     expect(request).toEqual(getSingleWithIncludes.kitsu)
+  })
+
+  // TODO: Support nested includes
+  /*
+  it('Should fetch a single resource with nested includes', async () => {
+    expect.assertions(1)
+    const api = new Kitsu()
+    mock.onGet().reply(200, getSingleWithNestedIncludes.jsonapi)
+    const request = await api.get('anime/1')
+    expect(request).toEqual(getSingleWithNestedIncludes.kitsu)
+  })
+  */
+
+  it('Should return a JSON:API error object for invalid queries', async () => {
+    expect.assertions(5)
+    const api = new Kitsu()
+    mock.onGet().reply(400, getError.jsonapi)
+    const { errors } = await api.get('articles', { include: 'author' })
+    expect(errors).toHaveLength(1)
+    expect(errors[0].title).toBe('Invalid field')
+    expect(errors[0].detail).toBeDefined()
+    expect(errors[0].code).toBeDefined()
+    expect(errors[0].status).toBe('400')
   })
 })
 
-describe('PATCH Requests', () => {
+describe('patch', () => {
+  it('should throw an error if Authorization header is not set (patch)', async () => {
+    expect.assertions(1)
+    const api = new Kitsu()
+    mock.onPatch().reply(200)
+    // expect(api.patch('posts', patchSingle.kitsu)).rejects.toThrow('Not logged in')
+    expect(api.patch('posts', patchSingle.kitsu)).rejects.toThrowError('Not logged in')
+  })
+
+  it('should throw an error if ID is missing (patch)', async () => {
+    expect.assertions(1)
+    const api = new Kitsu({ headers: { Authorization: true } })
+    mock.onPatch().reply(200)
+    expect(api.patch('posts', patchSingleMissingID.kitsu)).rejects.toThrowError('Updating a resource requires an ID')
+  })
+
+  it('Should update a single resource', async () => {
+    expect.assertions(1)
+    const api = new Kitsu({ headers: { Authorization: true } })
+    mock.onPatch().reply(200, patchSingle.jsonapi)
+    const request = await api.patch('posts', patchSingle.kitsu)
+    expect(request).toEqual(patchSingle.jsonapi)
+  })
+})
+
+describe('post', () => {
+  it('should throw an error if Authorization header is not set (post)', async () => {
+    expect.assertions(1)
+    const api = new Kitsu()
+    mock.onPost().reply(200)
+    expect(api.post('posts', patchSingle.kitsu)).rejects.toThrowError('Not logged in')
+  })
+})
+
+describe('delete', () => {
+  it('should throw an error if Authorization header is not set (delete)', async () => {
+    expect.assertions(1)
+    const api = new Kitsu()
+    mock.onDelete().reply(200)
+    expect(api.remove('posts', patchSingle.kitsu)).rejects.toThrowError('Not logged in')
+  })
+
+  /*
+  it('should throw an error if ID is missing (delete)', async () => {
+    expect.assertions(1)
+    const api = new Kitsu({ headers: { Authorization: true } })
+    mock.onDelete().reply(200)
+    expect(api.remove('posts', 1)).rejects.toThrowError('Deleting a resource requires an ID')
+  })
+  */
+})
+
+describe('self', () => {
 
 })
 
-describe('POST Requests', () => {
+describe('aliases', () => {
+  const api = new Kitsu()
 
-})
+  it('should alias fetch to get', () => {
+    expect.assertions(1)
+    expect(api.fetch).toEqual(api.get)
+  })
 
-describe('DELETE Requests', () => {
+  it('should alias update to patch', () => {
+    expect.assertions(1)
+    expect(api.update).toEqual(api.patch)
+  })
 
+  it('should alias create to post', () => {
+    expect.assertions(1)
+    expect(api.create).toEqual(api.post)
+  })
 })
