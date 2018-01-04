8f7fbebd06d3338c252f524e62d9b2dba205b34e
James Harris <wopian@wopian.me>
2018-01-04 01:31:23 +0000

    test: properly capture thrown error by filterIncludes

--- src/util/filterIncludes/index.spec.js
+++ src/util/filterIncludes/index.spec.js
@@ -1,7 +1,10 @@
 import { filterIncludes } from './'
 
 describe('filterIncludes', () => {
-  it('Should throw an error if included is not an array', () => {
-    expect(filterIncludes({}, { id: '1', type: 'anime' })).rejects.toEqual(new TypeError('included.filter is not a function'))
+  it('Should throw an error if included is not an array', async () => {
+    expect.assertions(1)
+    expect(filterIncludes({}, { id: '1', type: 'anime' }))
+      .rejects
+      .toThrowError('included.filter is not a function')
   })
 })
