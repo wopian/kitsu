47a7fbaf1283e0c6490255a71e967832a0a26359
James Harris <wopian@wopian.me>
2018-01-04 01:22:59 +0000

    test: cleanup deattribute tests

--- src/util/deattribute/index.spec.js
+++ src/util/deattribute/index.spec.js
@@ -1,52 +1,32 @@
 import { deattribute } from './'
 
+const attributesKey = {
+  jsonapi: { attributes: { key: 'value' } },
+  kitsu: { key: 'value' }
+}
+
 describe('deattribute', () => {
   it('Should deattribute an object', async () => {
     expect.assertions(1)
-    expect(await deattribute({
-      attributes: {
-        key: 'value'
-      }
-    })).toEqual({
-      key: 'value'
-    })
+    expect(await deattribute(attributesKey.jsonapi))
+      .toEqual(attributesKey.kitsu)
   })
 
   it('Should deattribute an array of objects', async () => {
     expect.assertions(1)
-    expect(await deattribute([
-      {
-        attributes: {
-          key: 'value'
-        }
-      },
-      {
-        attributes: {
-          key: 'value'
-        }
-      }
-    ])).toEqual([
-      {
-        key: 'value'
-      },
-      {
-        key: 'value'
-      }
-    ])
+    expect(await deattribute([attributesKey.jsonapi, attributesKey.jsonapi]))
+      .toEqual([attributesKey.kitsu, attributesKey.kitsu])
   })
 
   it('Should do nothing if no attributes supplied', async () => {
     expect.assertions(1)
-    expect(await deattribute({
-      id: '1'
-    })).toEqual({ id: '1' })
+    expect(await deattribute({ id: '1' }))
+      .toEqual({ id: '1' })
   })
 
   it('Should strip attributes object if empty', async () => {
     expect.assertions(1)
-    expect(await deattribute({
-      id: '1',
-      attributes: {}
-    })).toEqual({ id: '1' })
+    expect(await deattribute({ id: '1', attributes: {} }))
+      .toEqual({ id: '1' })
   })
 })
