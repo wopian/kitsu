b766c167d1ff1e115a88b7999b5f85eb1c2a407c
James Harris <wopian@wopian.me>
2018-01-05 16:46:04 +0000

    build: don't mangle variables in releases - removes need for sourcemap bloat

--- rollup.config.js
+++ rollup.config.js
@@ -3,94 +3,89 @@ import minify from 'rollup-plugin-babel-minify'
 import local from 'rollup-plugin-local-resolve'
 import pkg from './package.json'
 
-let external = Object.keys(pkg.dependencies)
-let plugins = [
-  babel({ exclude: [ '*.json', 'node_modules/**/*' ], runtimeHelpers: true }),
-  minify({ comments: false }),
+const onwarn = ({ code, message }) => {
+  // Suppress the following error message:
+  // https://github.com/rollup/rollup/wiki/Troubleshooting#this-is-undefined
+  if (code === 'THIS_IS_UNDEFINED') return
+  console.error(message)
+}
+
+let external = [
+  'babel-runtime/helpers/asyncToGenerator',
+  'babel-runtime/helpers/slicedToArray',
+  ...Object.keys(pkg.dependencies)
+]
+let externalLegacy = [
+  'babel-runtime/regenerator',
+  'babel-runtime/helpers/asyncToGenerator',
+  'babel-runtime/helpers/classCallCheck',
+  'babel-runtime/helpers/createClass',
+  'babel-runtime/helpers/slicedToArray',
+  'babel-runtime/helpers/typeof',
+  ...external
+]
+
+let pluginsCommon = [
+  minify({ comments: false, mangle: false }),
   local()
 ]
+let plugins = [
+  babel({ exclude: [ '*.json', 'node_modules/**/*' ], runtimeHelpers: true }),
+  ...pluginsCommon
+]
+let pluginsLegacy = [
+  babel({
+    exclude: [ '*.json', 'node_modules/**/*' ],
+    runtimeHelpers: true,
+    presets: [ [ 'env', { targets: { browsers: ['>= 0.1%'], node: 6 }, modules: false } ], 'stage-0' ]
+  }),
+  ...pluginsCommon
+]
+
 let globals = {
   'babel-runtime/helpers/asyncToGenerator': '_asyncToGenerator',
   'babel-runtime/helpers/slicedToArray': '_slicedToArray',
   'axios': 'axios'
 }
-let legacy = {
-  plugins: [
-    babel({
-      babelrc: false,
-      exclude: [ '*.json', 'node_modules/**/*' ],
-      runtimeHelpers: true,
-      presets: [ 'minify', [ 'env', { targets: { browsers: ['>= 0.1%'], node: 6 }, modules: false } ], 'stage-0' ],
-      plugins: [ [ 'transform-runtime', { polyfill: false, regenerator: true } ] ]
-    }),
-    minify({ comments: false }),
-    local()
-  ],
-  globals: {
-    'babel-runtime/regenerator': '_regeneratorRuntime',
-    'babel-runtime/helpers/classCallCheck': '_classCallCheck',
-    'babel-runtime/helpers/createClass': '_createClass',
-    'babel-runtime/helpers/typeof': '_typeof',
-    ...globals
-  }
+let globalsLegacy = {
+  'babel-runtime/regenerator': '_regeneratorRuntime',
+  'babel-runtime/helpers/classCallCheck': '_classCallCheck',
+  'babel-runtime/helpers/createClass': '_createClass',
+  'babel-runtime/helpers/typeof': '_typeof',
+  ...globals
 }
 
 export default [
   {
     input: 'src/index.js',
-    external: [
-      'babel-runtime/helpers/asyncToGenerator',
-      'babel-runtime/helpers/slicedToArray',
-      ...external
-    ],
+    external,
     plugins,
+    onwarn,
     output: [
       {
         file: pkg.main,
         format: 'umd',
         name: 'kitsu',
-        sourcemap: true,
         globals
       },
       {
         file: pkg.module,
         format: 'es',
-        sourcemap: true,
         globals
       }
-    ],
-    onwarn: ({ code, message }) => {
-      // Suppress the following error message:
-      // https://github.com/rollup/rollup/wiki/Troubleshooting#this-is-undefined
-      if (code === 'THIS_IS_UNDEFINED') return
-      console.error(message)
-    }
+    ]
   },
   {
     // Legacy IE10+ bundle
     input: 'src/index.js',
-    external: [
-      'babel-runtime/regenerator',
-      'babel-runtime/helpers/asyncToGenerator',
-      'babel-runtime/helpers/classCallCheck',
-      'babel-runtime/helpers/createClass',
-      'babel-runtime/helpers/slicedToArray',
-      'babel-runtime/helpers/typeof',
-      ...external
-    ],
-    plugins: legacy.plugins,
+    external: externalLegacy,
+    plugins: pluginsLegacy,
+    onwarn,
     output: {
       file: 'lib/legacy/index.js',
       format: 'umd',
       name: 'kitsu',
-      sourcemap: true,
-      globals: legacy.globals
-    },
-    onwarn: ({ code, message }) => {
-      // Suppress the following error message:
-      // https://github.com/rollup/rollup/wiki/Troubleshooting#this-is-undefined
-      if (code === 'THIS_IS_UNDEFINED') return
-      console.error(message)
+      globals: globalsLegacy
     }
   }
 ]
