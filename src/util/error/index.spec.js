0e27e8c827921c1bb7ad538e60ca2ef3d1a7829e
James Harris <wopian@wopian.me>
2018-01-15 04:38:01 +0000

    test: add tests for error function

---
+++ src/util/error/index.spec.js
@@ -0,0 +1,39 @@
+import { error } from './'
+
+describe('error', () => {
+  it('Should handle axios response errors', () => {
+    expect.assertions(1)
+    const obj = { response: {} }
+    expect(() => { error(obj) }).toThrow()
+  })
+
+  it('Should handle axios response errors with JSON:API errors', () => {
+    expect.assertions(1)
+    const obj = {
+      response: {
+        data: {
+          errors: [ { code: 400 } ]
+        }
+      }
+    }
+    expect(error(obj)).toEqual({
+      errors: [ { code: 400 } ]
+    })
+  })
+
+  it('Should handle top-level JSON:API errors', () => {
+    expect.assertions(1)
+    const obj = {
+      errors: [ { code: 400 } ]
+    }
+    expect(error(obj)).toEqual({
+      errors: [ { code: 400 } ]
+    })
+  })
+
+  it('Should throw all other errors', () => {
+    expect.assertions(2)
+    expect(() => { error('Hello') }).toThrowError('Hello')
+    expect(() => { error(new Error('Hello')) }).toThrowError('Hello')
+  })
+})
