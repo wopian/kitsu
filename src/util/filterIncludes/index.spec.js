8522a9641c0fa9aba61068879472bd0831d2041c
James Harris <wopian@wopian.me>
2018-01-15 04:38:44 +0000

    test: add test for filterIncludes branch

--- src/util/filterIncludes/index.spec.js
+++ src/util/filterIncludes/index.spec.js
@@ -7,4 +7,10 @@ describe('filterIncludes', () => {
       .rejects
       .toThrowError('included.filter is not a function')
   })
+
+  it('Should return id and type if included is empty', async () => {
+    expect.assertions(1)
+    const response = await filterIncludes([], { id: '1', type: 'comments' })
+    expect(response).toEqual({ id: '1', type: 'comments' })
+  })
 })
