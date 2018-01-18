ad9483228056813c62979850aada1951c4648057
Menthol <bennetteson@gmail.com>
2018-01-18 15:23:48 +0100

    fix: convert CamelCase into lowercase for kebab and snake conversions (#95)

--- src/util/snake/index.js
+++ src/util/snake/index.js
@@ -5,4 +5,4 @@
  * @returns {string} snake_case formatted string
  * @private
  */
-export default s => s.replace(/[A-Z\u00C0-\u00D6\u00D8-\u00DE]/g, match => '_' + match.toLowerCase())
+export default s => s.charAt(0).toLowerCase() + s.slice(1).replace(/[A-Z\u00C0-\u00D6\u00D8-\u00DE]/g, match => '_' + match.toLowerCase())
