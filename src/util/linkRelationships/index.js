b9762c159def8d9e1b9a1ad4f8e16727e3764b42
nath <nath@noizers.com>
2018-01-19 14:46:59 +0100

    feat: Implements nested relationships

--- src/util/linkRelationships/index.js
+++ src/util/linkRelationships/index.js
@@ -8,28 +8,36 @@ import { deattribute, error, filterIncludes } from '../'
  * @private
  */
 export async function linkRelationships (data, included) {
+  let removeRelationshipsAttribute = false
+
   try {
     const { relationships } = data
     for (let key in await relationships) {
       // Relationship contains collection of resources
       if (relationships[key].data && Array.isArray(relationships[key].data)) {
         for (let { id, type } of await relationships[key].data) {
-          const deattributed = await deattribute(await filterIncludes(included, { id, type }))
+          const filtered = await filterIncludes(included, { id, type })
+          if (filtered.relationships) await linkRelationships(filtered, included)
+          const deattributed = await deattribute(filtered)
           if (typeof deattributed !== 'undefined') {
             if (!data[key]) data[key] = []
             data[key].push(deattributed)
           }
         }
+        removeRelationshipsAttribute = true
       // Relationship contains a single resource
       } else if (relationships[key].data) {
         const { id, type } = relationships[key].data
-        const deattributed = await deattribute(await filterIncludes(included, { id, type }))
+        const filtered = await filterIncludes(included, { id, type })
+        if (filtered.relationships) await linkRelationships(filtered, included)
+        const deattributed = await deattribute(filtered)
         if (typeof deattributed !== 'undefined' && !data[key]) data[key] = deattributed
         delete data[key].relationships
+        removeRelationshipsAttribute = true
       }
     }
 
-    delete data.relationships
+    if (removeRelationshipsAttribute) delete data.relationships
 
     return data
   } catch (E) {
