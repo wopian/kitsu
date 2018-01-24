d4019919f1ecd8c78af57f8c55af333feead6ee6
James Harris <wopian@wopian.me>
2018-01-24 14:45:37 +0000

    refactor: inject JSON:API error array into top level of axios error

--- src/util/error/index.js
+++ src/util/error/index.js
@@ -1,7 +1,10 @@
 export function error (E) {
   if (E.response) {
     const e = E.response.data
-    if (e) return e
+    if (e && e.errors) {
+      E.errors = e.errors
+      return E
+    }
   } else if (E.errors) return E
   throw E
 }
