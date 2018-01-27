97a355641a4689165530d6ee446fc41cbf8df994
James Harris <wopian@wopian.me>
2018-01-27 05:34:40 +0000

    docs(error): add documentation

--- src/util/error/index.js
+++ src/util/error/index.js
@@ -1,10 +1,14 @@
+/**
+ * Mutates an error and rethrows it
+ *
+ * @param {Object} E The Error
+ * @throws {Object} The mutated Error
+ * @private
+ */
 export function error (E) {
   if (E.response) {
     const e = E.response.data
-    if (e && e.errors) {
-      E.errors = e.errors
-      return E
-    }
-  } else if (E.errors) return E
+    if (e && e.errors) E.errors = e.errors
+  }
   throw E
 }
