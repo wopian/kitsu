560196bdcfb2ed4fefc879827dc423767fd1c6e0
James Harris <wopian@wopian.me>
2018-02-17 20:14:14 +0000

    docs(kitsu-core): add example for snake function

--- packages/kitsu-core/src/snake/index.js
+++ packages/kitsu-core/src/snake/index.js
@@ -4,5 +4,8 @@
  * @name snake
  * @param {string} s camelCase string
  * @returns {string} snake_case formatted string
+ *
+ * @example
+ * snake('helloWorld') // 'hello_world'
  */
 export default s => s.charAt(0).toLowerCase() + s.slice(1).replace(/[A-Z\u00C0-\u00D6\u00D8-\u00DE]/g, match => '_' + match.toLowerCase())
