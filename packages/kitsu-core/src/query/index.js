fb167b2eba02ab805cf83bf06a2ed5c83066526e
James Harris <wopian@wopian.me>
2018-08-14 23:38:43 +0100

    refactor(kitsu-core): reduce query's cognitive complexity

--- packages/kitsu-core/src/query/index.js
+++ packages/kitsu-core/src/query/index.js
@@ -1,21 +1,29 @@
 /**
+ * Formats a single URL query
+ *
+ * @param {string} value Right-hand side of the query
+ * @param {string} key Left-hand side of the query
+ * @returns {string} URL query string
+ */
+function queryFormat (value, key) {
+  if (value !== null && typeof value === 'object') return query(value, key)
+  else return encodeURIComponent(key) + '=' + encodeURIComponent(value)
+}
+
+/**
  * Constructs a URL query string for JSON:API parameters
  *
  * @param {Object} params Parameters to parse
  * @param {string} prefix Prefix for nested parameters - used internally (default `null`)
  * @returns {string} URL query string
  */
-
 export function query (params, prefix = null) {
   const str = []
 
-  for (const p in params) {
-    if (params.hasOwnProperty(p)) {
-      const k = prefix ? `${prefix}[${p}]` : p
-      const v = params[p]
-      str.push((v !== null && typeof v === 'object')
-        ? query(v, k)
-        : encodeURIComponent(k) + '=' + encodeURIComponent(v)
+  for (const param in params) {
+    if (params.hasOwnProperty(param)) {
+      str.push(
+        queryFormat(params[param], prefix ? `${prefix}[${param}]` : param)
       )
     }
   }
