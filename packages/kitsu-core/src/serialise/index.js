593b2aff50a25b6e9a5909a0badec4c8f5cf3078
James Harris <wopian@wopian.me>
2018-08-15 03:28:50 +0100

    refactor(serialise ): reduce cognitive complexity

--- packages/kitsu-core/src/serialise/index.js
+++ packages/kitsu-core/src/serialise/index.js
@@ -3,6 +3,54 @@ import { error } from '../error'
 const requiresID = (method, key) => `${method} requires an ID for the ${key} relationships`
 
 /**
+ * Checks if data is valid for serialisation
+ *
+ * @param {Object} obj The data
+ * @param {string} method Request type
+ */
+async function isValid (obj, method, type) {
+  // Check if obj is not an object or empty
+  if (obj.constructor !== Object || Object.keys(obj).length === 0) {
+    throw new Error(`${method} requires a JSON object body`)
+  }
+  // A POST request is the only request to not require an ID
+  if (method !== 'POST' && !obj.id) {
+    throw new Error(`${method} requires an ID for the ${type} type`)
+  }
+}
+
+async function serialiseObject (node, nodeType, key, data, method) {
+  if (typeof node.id !== 'string') throw new Error(requiresID(method, key))
+  if (!data.relationships) data.relationships = {}
+  // Guess type if not provided
+  if (!node.type) node.type = nodeType
+  data.relationships[key] = {
+    data: Object.assign(node)
+  }
+  return data
+}
+
+async function serialiseArray (node, nodeType, key, data, method) {
+  if (!data.relationships) data.relationships = {}
+  data.relationships[key] = {
+    data: node.map(({ id, type }) => {
+      if (!id) throw new Error(requiresID(method, key))
+      return {
+        id,
+        type: type || nodeType
+      }
+    })
+  }
+  return data
+}
+
+async function serialiseAttr (node, key, data) {
+  if (!data.attributes) data.attributes = {}
+  data.attributes[key] = node
+  return data
+}
+
+/**
  * Serialises an object into a JSON-API structure
  *
  * @param {string} model Request model
@@ -10,7 +58,7 @@ const requiresID = (method, key) => `${method} requires an ID for the ${key} rel
  * @param {string} method Request type
  * @returns {Object} The serialised data
  *
- * @example <caption>Due to its usage in kitsu, it **MUST** be called with **this** set in 5.0.x</caption>
+ * @example <caption>Due to its usage in kitsu, it **MUST** be called with **this** set in 6.0.x</caption>
  * import { serialise, camel, kebab } from 'kitsu-core'
  * import plural from 'pluralize'
  *
@@ -18,50 +66,25 @@ const requiresID = (method, key) => `${method} requires an ID for the ${key} rel
  */
 export async function serialise (model, obj = {}, method = 'POST') {
   try {
-    // Check if obj is not an object or empty
-    if (obj.constructor !== Object || Object.keys(obj).length === 0) {
-      throw new Error(`${method} requires a JSON object body`)
-    }
     const type = this.plural(this.camel(model))
-    const data = { type }
+    let data = { type }
 
-    // A POST request is the only request to not require an ID
-    if (method !== 'POST' && !obj.id) {
-      throw new Error(`${method} requires an ID for the ${type} type`)
-    }
+    await isValid(obj, method, type)
 
-    // Add ID to data - MUST be a string
-    if (method !== 'POST') {
-      data.id = obj.id.toString()
-    }
+    if (method !== 'POST') data.id = String(obj.id)
 
-    // Attributes and relationships
     for (let key in obj) {
-      // Check if it's a relationship
-      if (obj[key] !== null && obj[key].constructor === Object) {
-        if (typeof obj[key].id === 'string') {
-          if (!data.relationships) data.relationships = {}
-          // Guess relationship type if not provided
-          if (!obj[key].type) obj[key].type = this.plural(this.camel(key))
-          data.relationships[key] = { data: Object.assign(obj[key]) }
-        } else throw new Error(requiresID(method, key))
-      // Check if it's a relationship array
-      } else if (obj[key] !== null && Array.isArray(obj[key])) {
-        // validate whole array
-        const keytype = this.plural(this.camel(key))
-        if (!data.relationships) data.relationships = {}
-        data.relationships[key] = { data: obj[key].map(elem => {
-          if (!elem.id) throw new Error(requiresID(method, key))
-          return {
-            id: elem.id,
-            type: elem.type || keytype
-          }
-        }) }
-      } else if (key !== 'id' && key !== 'type') { // Its an attribute
-        if (!data.attributes) data.attributes = {}
-        data.attributes[key] = obj[key]
+      const node = obj[key]
+      const nodeType = this.plural(this.camel(key))
+      if (node !== null & node.constructor === Object) {
+        data = await serialiseObject(node, nodeType, key, data, method)
+      } else if (node !== null && Array.isArray(node)) {
+        data = await serialiseArray(node, nodeType, key, data, method)
+      } else if (key !== 'id' && key !== 'type') {
+        data = await serialiseAttr(node, key, data)
       }
     }
+
     return { data }
   } catch (E) {
     throw error(E)
