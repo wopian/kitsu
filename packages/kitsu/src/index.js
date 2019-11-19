38e39f0627462c226dbcc6cf3450d18ceb650abe
Benjamin Griffith <griffith.ben@gmail.com>
2019-11-19 00:15:52 -0600

    alias interceptors from axios to Kitsu

--- packages/kitsu/src/index.js
+++ packages/kitsu/src/index.js
@@ -77,6 +77,7 @@ export default class Kitsu {
     this.update = this.patch
     this.create = this.post
     this.remove = this.delete
+    this.interceptors = this.axios.interceptors
   }
 
   /**
