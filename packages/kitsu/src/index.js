1d08ccec54cd4732fd4bb3ea959af41a3fd794d6
James Harris <wopian@wopian.me>
2019-11-22 02:01:58 +0000

    docs: move interceptors documentation to relevant line

--- packages/kitsu/src/index.js
+++ packages/kitsu/src/index.js
@@ -26,30 +26,6 @@ import { camel, deserialise, error, kebab, query, serialise, snake } from 'kitsu
  *     Authorization: 'Bearer 1234567890'
  *   }
  * })
- * @example <caption>Using Interceptors</caption>
- * // Add a request interceptor
- * api.interceptors.request.use(function (config) {
- *    // Do something before request is sent
- *    return config;
- * }, function (error) {
- *    // Do something with request error
- *    return Promise.reject(error);
- * });
- *
- * // Add a response interceptor
- * api.interceptors.response.use(function (response) {
- *    // Any status code that lie within the range of 2xx cause this function to trigger
- *    // Do something with response data
- *    return response;
- * }, function (error) {
- *    // Any status codes that falls outside the range of 2xx cause this function to trigger
- *    // Do something with response error
- *    return Promise.reject(error);
- * });
- *
- * // Remove an interceptor
- * const myInterceptor = api.interceptors.request.use(function () {...});
- * api.interceptors.request.eject(myInterceptor);
  */
 export default class Kitsu {
   constructor (options = {}) {
@@ -101,6 +77,37 @@ export default class Kitsu {
     this.update = this.patch
     this.create = this.post
     this.remove = this.delete
+    
+    /**
+     * Axios Interceptors (alias of `axios.interceptors`)
+     *
+     * You can intercept responses before they are handled by `get`, `post`, `patch` and `delete` and before requests are sent to the API server.
+     *
+     * @memberof Kitsu
+     * @example <caption>Request Interceptor</caption>
+     * // Add a request interceptor
+     * api.interceptors.request.use(config => {
+     *    // Do something before request is sent
+     *    return config;
+     * }, error => {
+     *    // Do something with request error
+     *    return Promise.reject(error);
+     * });
+     * @example <caption>Response Interceptor</caption>
+     * // Add a response interceptor
+     * api.interceptors.response.use(response => {
+     *    // Any status code that lie within the range of 2xx cause this function to trigger
+     *    // Do something with response data
+     *    return response;
+     * }, error => {
+     *    // Any status codes that falls outside the range of 2xx cause this function to trigger
+     *    // Do something with response error
+     *    return Promise.reject(error);
+     * });
+     * @example <caption>Removing Interceptors</caption>
+     * const myInterceptor = api.interceptors.request.use(function () {...});
+     * api.interceptors.request.eject(myInterceptor);
+     */
     this.interceptors = this.axios.interceptors
   }
 
