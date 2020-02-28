efa297c2c7d81afc32a1f616efc2ac6f1cc12fd6
Benjamin Griffith <griffith.ben@gmail.com>
2020-02-27 21:22:46 -0600

    feat(kitsu): deserialize POST and PATCH responses

--- packages/kitsu/src/index.js
+++ packages/kitsu/src/index.js
@@ -262,7 +262,7 @@ export default class Kitsu {
         { headers: Object.assign(this.headers, headers) }
       )
 
-      return data
+      return deserialise(data)
     } catch (E) {
       throw error(E)
     }
@@ -287,7 +287,7 @@ export default class Kitsu {
         headers: Object.assign(this.headers, headers)
       })
 
-      return data
+      return deserialise(data)
     } catch (E) {
       throw error(E)
     }
