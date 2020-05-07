63c15f4048bf0b30d41a7877daf41101f3b3ffd9
James Harris <wopian@wopian.me>
2020-05-07 12:49:37 +0100

    docs(kitsu-core): add example for query

--- packages/kitsu-core/src/query/index.js
+++ packages/kitsu-core/src/query/index.js
@@ -17,6 +17,18 @@ function queryFormat (value, key) {
  * @param {Object} params Parameters to parse
  * @param {string} prefix Prefix for nested parameters - used internally (default `null`)
  * @returns {string} URL query string
+ *
+ * @example
+ * query({
+ *   filter: {
+ *     slug: 'cowboy-bebop',
+ *     title: {
+ *       value: 'foo'
+ *     }
+ *   }
+ *  sort: '-id'
+ * })
+ * // filter%5Bslug%5D=cowboy-bebop&filter%5Btitle%5D%5Bvalue%5D=foo&sort=-id
  */
 export function query (params, prefix = null) {
   const str = []
