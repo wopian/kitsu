ff05659243c024afe9a4537a480fd59a26b9ba3d
James Harris <wopian@wopian.me>
2020-05-07 13:07:51 +0100

    docs(kitsu-core): add examples for linkRelationships

--- packages/kitsu-core/src/linkRelationships/index.js
+++ packages/kitsu-core/src/linkRelationships/index.js
@@ -71,6 +71,29 @@ function linkAttr (data, included, key) {
  *
  * @param {Object} data The response data object
  * @param {Object} included The response included object
+ * @returns Parsed data
+ *
+ * @example
+ * const data = {
+ *   attributes: { author: 'Joe' },
+ *   relationships: {
+ *     author: {
+ *       data: { id: '1', type: 'people' }
+ *     }
+ *   }
+ * }
+ * const included = [ {
+ *   id: '1',
+ *   type: 'people',
+ *   attributes: { name: 'Joe' }
+ * } ]
+ * const output = linkRelationships(data, included)
+ * // {
+ * //   attributes: { author: 'Joe' },
+ * //   author: {
+ * //     data: { id: '1', name: 'Joe', type: 'people' }
+ * //   }
+ * // }
  */
 export function linkRelationships (data, included = []) {
   const { relationships } = data
