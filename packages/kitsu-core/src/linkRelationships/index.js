1aa50139b36c3ad87fd9a6aad568efe1936db54b
James Harris <james.harris@trutify.com>
2022-03-04 11:14:05 +0000

    chore: guard against null

--- packages/kitsu-core/src/linkRelationships/index.js
+++ packages/kitsu-core/src/linkRelationships/index.js
@@ -109,7 +109,7 @@ export function linkRelationships (data, included = []) {
     }
   }
 
-  if (Object.keys(relationships || []).length === 0 && typeof relationships === 'object') {
+  if (Object.keys(relationships || []).length === 0 && typeof relationships === 'object' && relationships !== null) {
     delete data.relationships
   }
 
