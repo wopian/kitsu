8d3970746a7d11b6805739d226effe4438e547df
James Harris <james.harris@trutify.com>
2022-03-04 11:27:12 +0000

    chore: guard against arrays

--- packages/kitsu-core/src/linkRelationships/index.js
+++ packages/kitsu-core/src/linkRelationships/index.js
@@ -120,7 +120,7 @@ export function linkRelationships (data, included = [], previouslyLinked = {}) {
     }
   }
 
-  if (Object.keys(relationships || []).length === 0 && typeof relationships === 'object' && relationships !== null) {
+  if (Object.keys(relationships || []).length === 0 && typeof relationships === 'object' && !Array.isArray(relationships) && relationships !== null) {
     delete data.relationships
   }
 
