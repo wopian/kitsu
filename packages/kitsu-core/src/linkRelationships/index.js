95d3453e61ac9883c6dcb30b15402ddae3587019
pedep <pedep@users.noreply.github.com>
2022-08-11 14:30:29 +0200

    fix(kitsu-core): fix inability to link relationship links on circular resources (#699)
    * fix(kitsu-core): fix inability to link relationship links on circular resources
    
    * replace repeating keystring with variable
    
    Co-authored-by: James Harris <wopian@wopian.me>
    
    * use optional chaining for shorter expressions
    
    * implement array resource linkage testcase
    
    * link meta key on relationship objects in linkArray
    
    Co-authored-by: James Harris <wopian@wopian.me>

--- packages/kitsu-core/src/linkRelationships/index.js
+++ packages/kitsu-core/src/linkRelationships/index.js
@@ -9,15 +9,16 @@ import { filterIncludes } from '../filterIncludes'
  * @param {string} resource.type Resource type
  * @param {Object} [resource.meta] Meta information
  * @param {Object[]} included The response included object
- * @param {Object} [previouslyLinked] A mapping of already visited resources
+ * @param {Object} previouslyLinked A mapping of already visited resources
+ * @param {Object} relationshipCache A cache object for relationship meta and links
  * @private
  */
-function link ({ id, type, meta }, included, previouslyLinked) {
+function link ({ id, type, meta }, included, previouslyLinked, relationshipCache) {
   const filtered = filterIncludes(included, { id, type })
   previouslyLinked[`${type}#${id}`] = filtered
 
   if (filtered.relationships) {
-    linkRelationships(filtered, included, previouslyLinked)
+    linkRelationships(filtered, included, previouslyLinked, relationshipCache)
   }
   if (meta) filtered.meta = meta
 
@@ -31,18 +32,20 @@ function link ({ id, type, meta }, included, previouslyLinked) {
  * @param {Object[]} included The response included object
  * @param {string} key Name of the relationship item
  * @param {Object} previouslyLinked A mapping of already visited resources
+ * @param {Object} relationshipCache A cache object for relationship meta and links
  * @private
  */
-function linkArray (data, included, key, previouslyLinked) {
+function linkArray (data, included, key, previouslyLinked, relationshipCache) {
   data[key] = {}
 
   if (data.relationships[key].links) data[key].links = data.relationships[key].links
+  if (data.relationships[key].meta) data[key].meta = data.relationships[key].meta
 
   data[key].data = []
 
   for (const resource of data.relationships[key].data) {
     const cache = previouslyLinked[`${resource.type}#${resource.id}`]
-    let relationship = cache || link(resource, included, previouslyLinked)
+    let relationship = cache || link(resource, included, previouslyLinked, relationshipCache)
     if (resource.meta !== relationship.meta) relationship = { ...relationship, meta: resource.meta }
     data[key].data.push(relationship)
   }
@@ -57,14 +60,22 @@ function linkArray (data, included, key, previouslyLinked) {
  * @param {Object[]} included The response included object
  * @param {string} key Name of the relationship item
  * @param {Object} previouslyLinked A mapping of already visited resources
+ * @param {Object} relationshipCache A cache object for relationship meta and links
  * @private
  */
-function linkObject (data, included, key, previouslyLinked) {
+function linkObject (data, included, key, previouslyLinked, relationshipCache) {
   data[key] = {}
   const resource = data.relationships[key].data
   const cache = previouslyLinked[`${resource.type}#${resource.id}`]
-  data[key].data = cache || link(resource, included, previouslyLinked)
-  if (data.relationships[key].links) data[key].links = data.relationships[key].links
+  data[key].data = cache || link(resource, included, previouslyLinked, relationshipCache)
+
+  const cacheKey = `${data.type}#${data.id}#${key}`
+  const relationships = relationshipCache[cacheKey] || data.relationships[key]
+  if (!relationshipCache[cacheKey]) relationshipCache[cacheKey] = relationships
+
+  if (relationships?.links) data[key].links = relationships.links
+  if (relationships?.meta) data[key].meta = relationships.meta
+
   delete data.relationships[key]
 }
 
@@ -87,6 +98,7 @@ function linkAttr (data, key) {
  * @param {Object} data The response data object
  * @param {Object[]} [included] The response included object
  * @param {Object} [previouslyLinked] A mapping of already visited resources (internal use only)
+ * @param {Object} [relationshipCache] A cache object for relationship meta and links
  * @returns Parsed data
  *
  * @example
@@ -111,16 +123,16 @@ function linkAttr (data, key) {
  * //   }
  * // }
  */
-export function linkRelationships (data, included = [], previouslyLinked = {}) {
+export function linkRelationships (data, included = [], previouslyLinked = {}, relationshipCache = {}) {
   const { relationships } = data
 
   for (const key in relationships) {
     // Relationship contains collection of resources
     if (Array.isArray(relationships[key]?.data)) {
-      linkArray(data, included, key, previouslyLinked)
+      linkArray(data, included, key, previouslyLinked, relationshipCache)
     // Relationship contains a single resource
     } else if (relationships[key].data) {
-      linkObject(data, included, key, previouslyLinked)
+      linkObject(data, included, key, previouslyLinked, relationshipCache)
     } else {
       linkAttr(data, key)
     }
