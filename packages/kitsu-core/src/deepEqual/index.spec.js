3db5e42f03471da4cc755de8bd751b7dc968c75d
wopian <wopian@wopian.me>
2022-10-30 17:33:22 +0000

    test(deepEqual): add additional tests to cover uncovered branches

--- packages/kitsu-core/src/deepEqual/index.spec.js
+++ packages/kitsu-core/src/deepEqual/index.spec.js
@@ -1,33 +1,63 @@
 import { isDeepEqual } from './'
 
 describe('kitsu-core', () => {
-  describe('IsDeepEqual', () => {
-    const person1 = {
-      firstName: 'John',
-      lastName: 'Doe',
-      age: 35
+  describe('isDeepEqual', () => {
+    const people = {
+      one: {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 35
+      },
+      two: {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 35
+      },
+      three: {
+        firstName: 'Akash',
+        lastName: 'Thakur',
+        age: 35
+      },
+      four: {
+        firstName: 'Jane',
+        lastName: 'Doe'
+      },
+      five: {
+        address: {
+          street: '123 Main St'
+        }
+      },
+      six: {
+        address: {
+          street: '123 Main St'
+        }
+      },
+      seven: {
+        address: {
+          street: '456 Main St'
+        }
+      }
     }
 
-    const person2 = {
-      firstName: 'John',
-      lastName: 'Doe',
-      age: 35
-    }
-
-    const person3 = {
-      firstName: 'Akash',
-      lastName: 'Thakur',
-      age: 35
-    }
-
-    it('Equality check for two same collections', () => {
+    it('checks indentical objects are equal', () => {
       expect.assertions(1)
-      expect(isDeepEqual(person1, person2)).toBe(true)
+      expect(isDeepEqual(people.one, people.two)).toBe(true)
     })
 
-    it('Equality check for two different collections', () => {
+    it('checks different objects are not equal', () => {
       expect.assertions(1)
-      expect(isDeepEqual(person1, person3)).toBe(false)
+      expect(isDeepEqual(people.one, people.three)).toBe(false)
+    })
+
+    it('checks objects have the same number of keys', () => {
+      expect.assertions(1)
+      expect(isDeepEqual(people.one, people.four)).toBe(false)
+    })
+
+    it('checks nested objects are equal', () => {
+      expect.assertions(2)
+      expect(isDeepEqual(people.five, people.six)).toBe(true)
+      expect(isDeepEqual(people.five, people.seven)).toBe(false)
     })
   })
 })
