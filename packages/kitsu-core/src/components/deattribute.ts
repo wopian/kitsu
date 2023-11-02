6c562754aad8a071bfd6539fbf5a75f0e2e14fda
Vincent Plannthin <vincent@plannthin.net>
2023-11-02 14:06:31 +0100

    refactor deattribute using new interfaces and type guards

--- packages/kitsu-core/src/components/deattribute.ts
+++ packages/kitsu-core/src/components/deattribute.ts
@@ -1,37 +1,26 @@
-interface Data {
-  id: string
-  type: string
-  attributes?: {
-    [key: string]:
-      | string
-      | number
-      | boolean
-      | null
-      | undefined
-      | object
-      | object[]
-      | string[]
-      | number[]
-      | boolean[]
-  }
-}
+import { Attributes, isAttributes } from "../resources/attributes.js";
+import { ResourceIdentifier } from "../resources/resource-identifier.js";
+import { ResourceObject } from "../resources/resource-object.js";
+
+export type DeattributedResourceObject = ResourceIdentifier & Attributes;
 
 // Write a function that hoists the attributes of a given object to the top level
-export const deattribute = (data: Data | Data[]): Data | Data[] => {
-  let output = data
-  if (Array.isArray(data)) output = data.map(deattribute) as Data[]
-  else if (
-    typeof data.attributes === 'object' &&
-    data.attributes !== null &&
-    !Array.isArray(data.attributes)
-  ) {
-    output = {
-      ...data,
-      ...data.attributes
-    } as Data
+export function deattribute(data: ResourceObject): DeattributedResourceObject;
+export function deattribute(data: ResourceObject[]): DeattributedResourceObject[];
+export function deattribute(data: ResourceObject | ResourceObject[]): DeattributedResourceObject | DeattributedResourceObject[] {
+  return isResourceObjectArray(data) ? data.map(_deattribute) : _deattribute(data);
+}
 
-    if (output.attributes === data.attributes) delete output.attributes
+function _deattribute(data: ResourceObject): DeattributedResourceObject {
+  const output = {
+    ...data,
+    ...data.attributes
   }
 
-  return output
+  if (output.attributes === data.attributes) delete output.attributes
+  return output;
+}
+
+function isResourceObjectArray(obj: ResourceObject | ResourceObject[]): obj is ResourceObject[] {
+  return Array.isArray(obj)
 }
