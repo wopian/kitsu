3d5dd0d645ed8a2c25abcb4df55aee73f32fc577
Vincent Plannthin <vincent@plannthin.net>
2023-11-02 14:05:34 +0100

    implement a few interfaces from the specification

---
+++ packages/kitsu-core/src/resources/resource-identifier.ts
@@ -0,0 +1,18 @@
+// https://jsonapi.org/format/#document-resource-object-identification
+//
+// > As noted above, every resource object MUST contain a type member.
+// > Every resource object MUST also contain an id member, except when the resource object originates at the client and represents a new resource to be created on the server.
+// > If id is omitted due to this exception, a lid member MAY be included to uniquely identify the resource by type locally within the document.
+// > The value of the lid member MUST be identical for every representation of the resource in the document, including resource identifier objects.
+
+export interface LocalResourceIdentifier {
+  lid: string;
+  type: string;
+}
+
+export interface RemoteResourceIdentifier {
+  id: string;
+  type: string;
+}
+
+export type ResourceIdentifier = LocalResourceIdentifier | RemoteResourceIdentifier;
